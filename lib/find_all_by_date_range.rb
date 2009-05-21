69f2bcaacc4196b358be8fde5bda1e6b30314b15
timmatheson <me@timmatheson.com>
2009-05-21 00:37:27 -0700

    adding documentation

--- lib/find_all_by_date_range.rb
+++ lib/find_all_by_date_range.rb
@@ -1,6 +1,9 @@
 module FindAllByDateRange
   class ActiveRecord::Base
-    def self.find_all_by_date_range(from,to,options={})
+    # Finds a set of records within a date range
+    # Example:
+    # User.find_all_by_date_range(Time.now-1.day, Time.now, :use => "signed_up_at")
+    def self.find_all_by_date_range(from,to,options={})#:doc
       options[:use].blank? ? use_column = :created_at : use_column = options.delete(:use).to_sym
       find(:all, (options || {}).merge(:conditions => {use_column => from.to_date.to_s(:db)..to.to_date.to_s(:db)}))
     end
